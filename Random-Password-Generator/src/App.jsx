e4af9eda41dcaf5a4f98881ef9fa6efb90be65ed
Ravi Dharigond <66740454+Ravidharigond1997@users.noreply.github.com>
2024-04-02 00:59:42 +0530

    RandomPassword Generattor new react project is getStarted

---
+++ Random-Password-Generator/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+import { useCallback } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, password]);
+
+  return (
+    <>
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+    </>
+  );
+}
+
+export default App;
