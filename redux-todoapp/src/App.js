7081c056e0cb998024940ebf255739a69286a449
Ravi Dharigond <66740454+Ravidharigond1997@users.noreply.github.com>
2024-05-02 00:33:15 +0530

    redux tool kit is get done

--- redux-todoapp/src/App.js
+++ redux-todoapp/src/App.js
@@ -1,8 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
 }
 
 export default App;
